28f8353afede67889b2139efa01570ce41fa74c9
Aleck <aleck.dev.git@.com>
2022-01-08 09:19:24 -0800

    responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,49 +1,53 @@
 import { DOCUMENT } from '@angular/common';
 import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 
-declare var navbar_js : any;
-function test(){
+declare var navbar_js: any;
+function test() {
   const nav = document.getElementById('navbar');
   console.log(nav);
-  
+
   window.addEventListener('scroll', () => {
-      console.log("test");
+    console.log('test');
   });
 }
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements AfterViewInit {
   navy = 'navy';
-  constructor(@Inject(DOCUMENT) private document: any) { 
-    
-  }
+  constructor(@Inject(DOCUMENT) private document: any) {}
 
   ngAfterViewInit() {
-    
-    const navbar  = document.querySelector('.navbar') ;
-
-    if(window.scrollY<100){
-      navbar?.setAttribute("style", "background-color: transparent;");
-      navbar?.classList .remove("navbar_scrolled");
-    }else{
-      navbar?.classList .add("navbar_scrolled");
+    const navbar = document.querySelector('.navbar');
+    const collapse = document.querySelector('.navbar-toggler');
+    const icon = document.querySelector('.navbar-toggler-icon');
+    icon?.addEventListener('click', () => {
+      if (collapse?.getAttribute('aria-expanded') == 'true') {
+        navbar?.setAttribute('style', 'background-color: #f4623a;');
+      } else {
+        navbar?.setAttribute('style', 'background-color: transparent;');
+      }
+    });
+    if (window.scrollY < 100) {
+      navbar?.setAttribute('style', 'background-color: transparent;');
+      navbar?.classList.remove('navbar_scrolled');
+    } else {
+      navbar?.classList.add('navbar_scrolled');
     }
 
-    window.addEventListener('scroll',(e)=>{
+    window.addEventListener('scroll', (e) => {
       console.log(e);
-      
-    let scroll =  window.scrollY;
-    console.log(scroll);
-    if(scroll<150){
-      navbar?.setAttribute("style", "background-color: transparent;");
-      navbar?.classList .remove("navbar_scrolled");
-    }else{
-      navbar?.classList .add("navbar_scrolled");
-    }
-    })
-  }
 
+      let scroll = window.scrollY;
+      console.log(scroll);
+      if (scroll < 150) {
+        navbar?.setAttribute('style', 'background-color: transparent;');
+        navbar?.classList.remove('navbar_scrolled');
+      } else {
+        navbar?.classList.add('navbar_scrolled');
+      }
+    });
+  }
 }
