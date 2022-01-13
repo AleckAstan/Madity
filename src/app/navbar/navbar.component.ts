494c9e95b8f63279625eaf0d9ff586db7dcebb05
Aleck <aleck.dev.git@.com>
2022-01-13 10:05:45 -0800

    mobile bug + hideOnScroll

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -16,26 +16,20 @@ function test() {
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements AfterViewInit {
-  navy = 'navy';
   constructor(@Inject(DOCUMENT) private document: any) {}
 
   ngAfterViewInit() {
     const navbar = document.querySelector('.navbar');
     const collapse = document.querySelector('.navbar-toggler');
     const icon = document.querySelector('.navbar-toggler-icon');
-    icon?.addEventListener('click', () => {
-      if (collapse?.getAttribute('aria-expanded') == 'true') {
-        navbar?.setAttribute('style', 'background-color: #f4623a;');
-      } else {
-        navbar?.setAttribute('style', 'background-color: transparent;');
-      }
-    });
-    if (window.scrollY < 100) {
-      navbar?.setAttribute('style', 'background-color: transparent;');
-      navbar?.classList.remove('navbar_scrolled');
-    } else {
-      navbar?.classList.add('navbar_scrolled');
-    }
+    const main_nav = document.getElementById('main_nav');
+
+    // if (window.scrollY < 100) {
+    //   navbar?.setAttribute('style', 'background-color: transparent;');
+    //   navbar?.classList.remove('navbar_scrolled');
+    // } else {
+    //   navbar?.classList.add('navbar_scrolled');
+    // }
 
     window.addEventListener('scroll', () => {
       // console.log(e);
@@ -49,5 +43,27 @@ export class NavbarComponent implements AfterViewInit {
         navbar?.classList.add('navbar_scrolled');
       }
     });
+    icon?.addEventListener('click', () => {
+      if (collapse?.getAttribute('aria-expanded') == 'true') {
+        navbar?.setAttribute('style', 'background-color: #f4623a;');
+      } else {
+        navbar?.setAttribute('style', 'background-color: transparent;');
+      }
+    });
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+        navbar?.setAttribute('style', 'top:0');
+        main_nav?.classList.remove('show');
+        // navbar.style.top = "0";
+      } else {
+        // navbar.style.top = "-50px";
+        main_nav?.classList.remove('show');
+        navbar?.setAttribute('style', 'top:-5rem');
+      }
+      prevScrollpos = currentScrollPos;
+    };
   }
 }
